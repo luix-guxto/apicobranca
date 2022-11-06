d4ecd0a7eb70859f39b67a9dd426a833a79f7c9f
luix-guxto <luisaugustoway@gmail.com>
2022-11-06 17:49:09 -0300

    codigo de barras add

--- conversaoJson.py
+++ conversaoJson.py
@@ -1,27 +1,30 @@
 import openpyxl as op
 import json
 import os
+import datetime as dt
 
 # Carrega o arquivo de planilha
 planilha = op.load_workbook('cobranca.xlsx')
 aba = planilha.active
 t = aba.max_row
+print('linhas ', t)
 a = 2
 clientes = {}
-while a < t+1:
-
+while a < t:
+    print('linha ', a, '\n')
     nome = aba.cell(row=a, column=1).value
-    vecimento = aba.cell(row=a, column=2).value
-    vecimento = vecimento.strftime('%d/%m/%Y')
+    vencimento = aba.cell(row=a, column=2).value
+    vencimento = vencimento.strftime('%d/%m')
     telefone = aba.cell(row=a, column=3).value
     cdbarras = aba.cell(row=a, column=4).value
     y = {
         'nome': nome,
-        'vencimento': vecimento,
+        'vencimento': vencimento,
         'telefone': telefone,
         'codbarras': cdbarras
     }
     clientes[a-2] = y
+    print(y)
     a += 1
 
 if os.path.exists('cobranca.json'):
